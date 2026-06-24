9bf86e3e6ec2169b725397fde70408e7f55aca4e
qile <jllyzjs@126.com>
2026-06-24 14:59:04 +0800

    Add files via upload

---
+++ app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingPlayerFragment.java
@@ -0,0 +1,291 @@
+package com.fongmi.android.tv.ui.fragment;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
+import androidx.viewbinding.ViewBinding;
+
+import com.fongmi.android.tv.R;
+import com.fongmi.android.tv.databinding.FragmentSettingPlayerBinding;
+import com.fongmi.android.tv.impl.BufferListener;
+import com.fongmi.android.tv.impl.SpeedListener;
+import com.fongmi.android.tv.impl.UaListener;
+import com.fongmi.android.tv.setting.PlayerSetting;
+import com.fongmi.android.tv.setting.Setting;
+import com.fongmi.android.tv.ui.base.BaseFragment;
+import com.fongmi.android.tv.ui.dialog.BufferDialog;
+import com.fongmi.android.tv.ui.dialog.LutDialog;
+import com.fongmi.android.tv.ui.dialog.SpeedDialog;
+import com.fongmi.android.tv.ui.dialog.UaDialog;
+import com.fongmi.android.tv.player.lut.LutSetting;
+import com.fongmi.android.tv.utils.ResUtil;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.text.DecimalFormat;
+
+public class SettingPlayerFragment extends BaseFragment implements UaListener, BufferListener, SpeedListener {
+
+    private static final String KEY_SHOW_RESOLUTION = "show_resolution";
+
+    private FragmentSettingPlayerBinding mBinding;
+    private DecimalFormat format;
+    private String[] background;
+    private String[] backBuffer;
+    private String[] bufferBytes;
+    private String[] caption;
+    private String[] kernel;
+    private String[] playCache;
+    private String[] render;
+    private String[] scale;
+    private String[] osdItems; // 自定义OSD选项列表（含“分辨率”）
+
+    public static SettingPlayerFragment newInstance() {
+        return new SettingPlayerFragment();
+    }
+
+    private String getSwitch(boolean value) {
+        return getString(value ? R.string.setting_on : R.string.setting_off);
+    }
+
+    @Override
+    protected ViewBinding getBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        return mBinding = FragmentSettingPlayerBinding.inflate(inflater, container, false);
+    }
+
+    @Override
+    protected void initView() {
+        format = new DecimalFormat("0.#");
+        mBinding.uaText.setText(Setting.getUa());
+        mBinding.aacText.setText(getSwitch(PlayerSetting.isPreferAAC()));
+        mBinding.tunnelText.setText(getSwitch(PlayerSetting.isTunnel()));
+        mBinding.adblockText.setText(getSwitch(Setting.isAdblock()));
+        mBinding.speedText.setText(format.format(PlayerSetting.getSpeed()));
+        mBinding.bufferText.setText(String.valueOf(PlayerSetting.getBuffer()));
+        mBinding.bufferBytesText.setText((bufferBytes = ResUtil.getStringArray(R.array.select_buffer_bytes))[PlayerSetting.getBufferBytesOption()]);
+        mBinding.backBufferText.setText((backBuffer = ResUtil.getStringArray(R.array.select_back_buffer))[PlayerSetting.getBackBufferOption()]);
+        mBinding.playCacheText.setText((playCache = ResUtil.getStringArray(R.array.select_play_cache))[PlayerSetting.getPlayCacheOption()]);
+        mBinding.autoChangeText.setText(getSwitch(PlayerSetting.isAutoChange()));
+        mBinding.audioDecodeText.setText(getSwitch(PlayerSetting.isAudioPrefer()));
+        mBinding.videoDecodeText.setText(getSwitch(PlayerSetting.isVideoPrefer()));
+        mBinding.caption.setVisibility(PlayerSetting.hasCaption() ? View.VISIBLE : View.GONE);
+
+        // 构建包含“分辨率”的OSD选项列表
+        osdItems = new String[]{"标题", "分辨率", "时间", "进度", "网速", "迷你进度"};
+        mBinding.osdText.setText(getOsdText(osdItems));
+
+        mBinding.kernelText.setText((kernel = ResUtil.getStringArray(R.array.select_player_kernel))[PlayerSetting.getPlayer()]);
+        mBinding.scaleText.setText((scale = ResUtil.getStringArray(R.array.select_scale))[PlayerSetting.getScale()]);
+        mBinding.lutText.setText(LutSetting.getSummary());
+        mBinding.renderText.setText((render = ResUtil.getStringArray(R.array.select_render))[PlayerSetting.getRender()]);
+        mBinding.captionText.setText((caption = ResUtil.getStringArray(R.array.select_caption))[PlayerSetting.isCaption() ? 1 : 0]);
+        mBinding.backgroundText.setText((background = ResUtil.getStringArray(R.array.select_background))[PlayerSetting.getBackground()]);
+    }
+
+    @Override
+    protected void initEvent() {
+        mBinding.ua.setOnClickListener(this::onUa);
+        mBinding.aac.setOnClickListener(this::setAAC);
+        mBinding.kernel.setOnClickListener(this::onKernel);
+        mBinding.scale.setOnClickListener(this::onScale);
+        mBinding.lut.setOnClickListener(this::onLut);
+        mBinding.osd.setOnClickListener(this::onOsd);
+        mBinding.speed.setOnClickListener(this::onSpeed);
+        mBinding.buffer.setOnClickListener(this::onBuffer);
+        mBinding.bufferBytes.setOnClickListener(this::onBufferBytes);
+        mBinding.backBuffer.setOnClickListener(this::onBackBuffer);
+        mBinding.playCache.setOnClickListener(this::onPlayCache);
+        mBinding.autoChange.setOnClickListener(this::setAutoChange);
+        mBinding.render.setOnClickListener(this::setRender);
+        mBinding.tunnel.setOnClickListener(this::setTunnel);
+        mBinding.caption.setOnClickListener(this::setCaption);
+        mBinding.adblock.setOnClickListener(this::setAdblock);
+        mBinding.caption.setOnLongClickListener(this::onCaption);
+        mBinding.background.setOnClickListener(this::onBackground);
+        mBinding.audioDecode.setOnClickListener(this::setAudioDecode);
+        mBinding.videoDecode.setOnClickListener(this::setVideoDecode);
+    }
+
+    private void onUa(View view) {
+        UaDialog.show(this);
+    }
+
+    @Override
+    public void setUa(String ua) {
+        mBinding.uaText.setText(ua);
+        Setting.putUa(ua);
+    }
+
+    private void setAAC(View view) {
+        PlayerSetting.putPreferAAC(!PlayerSetting.isPreferAAC());
+        mBinding.aacText.setText(getSwitch(PlayerSetting.isPreferAAC()));
+    }
+
+    private void onKernel(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_kernel).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(kernel, PlayerSetting.getPlayer(), (dialog, which) -> {
+            mBinding.kernelText.setText(kernel[which]);
+            PlayerSetting.putPlayer(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void onScale(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_scale).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(scale, PlayerSetting.getScale(), (dialog, which) -> {
+            mBinding.scaleText.setText(scale[which]);
+            PlayerSetting.putScale(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void onLut(View view) {
+        LutDialog.show(this, () -> mBinding.lutText.setText(LutSetting.getSummary()));
+    }
+
+    private void onOsd(View view) {
+        boolean[] checked = getOsdChecked();
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_osd).setNegativeButton(R.string.dialog_negative, null).setPositiveButton(R.string.dialog_positive, (dialog, which) -> {
+            setOsdChecked(checked);
+            mBinding.osdText.setText(getOsdText(osdItems));
+        }).setMultiChoiceItems(osdItems, checked, (dialog, which, isChecked) -> checked[which] = isChecked).show();
+    }
+
+    private boolean[] getOsdChecked() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        return new boolean[]{
+                PlayerSetting.isOsdTitle(),
+                prefs.getBoolean(KEY_SHOW_RESOLUTION, true),
+                PlayerSetting.isOsdTime(),
+                PlayerSetting.isOsdProgress(),
+                PlayerSetting.isOsdTraffic(),
+                PlayerSetting.isOsdMini()
+        };
+    }
+
+    private void setOsdChecked(boolean[] checked) {
+        PlayerSetting.putOsdTitle(checked[0]);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(requireContext()).edit();
+        editor.putBoolean(KEY_SHOW_RESOLUTION, checked[1]);
+        editor.apply();
+        PlayerSetting.putOsdTime(checked[2]);
+        PlayerSetting.putOsdProgress(checked[3]);
+        PlayerSetting.putOsdTraffic(checked[4]);
+        PlayerSetting.putOsdMini(checked[5]);
+    }
+
+    private String getOsdText(String[] items) {
+        boolean[] checked = getOsdChecked();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < checked.length; i++) {
+            if (!checked[i]) continue;
+            if (builder.length() > 0) builder.append(" / ");
+            builder.append(items[i]);
+        }
+        return builder.length() == 0 ? getString(R.string.setting_off) : builder.toString();
+    }
+
+    private void onSpeed(View view) {
+        SpeedDialog.show(this);
+    }
+
+    @Override
+    public void setSpeed(float speed) {
+        mBinding.speedText.setText(format.format(speed));
+        PlayerSetting.putSpeed(speed);
+    }
+
+    private void onBuffer(View view) {
+        BufferDialog.show(this);
+    }
+
+    @Override
+    public void setBuffer(int times) {
+        mBinding.bufferText.setText(String.valueOf(times));
+        PlayerSetting.putBuffer(times);
+    }
+
+    private void onBufferBytes(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_buffer_bytes).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(bufferBytes, PlayerSetting.getBufferBytesOption(), (dialog, which) -> {
+            mBinding.bufferBytesText.setText(bufferBytes[which]);
+            PlayerSetting.putBufferBytesOption(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void onBackBuffer(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_back_buffer).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(backBuffer, PlayerSetting.getBackBufferOption(), (dialog, which) -> {
+            mBinding.backBufferText.setText(backBuffer[which]);
+            PlayerSetting.putBackBufferOption(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void onPlayCache(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_cache).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(playCache, PlayerSetting.getPlayCacheOption(), (dialog, which) -> {
+            mBinding.playCacheText.setText(playCache[which]);
+            PlayerSetting.putPlayCacheOption(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void setAutoChange(View view) {
+        PlayerSetting.putAutoChange(!PlayerSetting.isAutoChange());
+        mBinding.autoChangeText.setText(getSwitch(PlayerSetting.isAutoChange()));
+    }
+
+    private void setRender(View view) {
+        if (PlayerSetting.isTunnel() && PlayerSetting.getRender() == 0) setTunnel(view);
+        int index = (PlayerSetting.getRender() + 1) % render.length;
+        mBinding.renderText.setText(render[index]);
+        PlayerSetting.putRender(index);
+    }
+
+    private void setTunnel(View view) {
+        PlayerSetting.putTunnel(!PlayerSetting.isTunnel());
+        mBinding.tunnelText.setText(getSwitch(PlayerSetting.isTunnel()));
+        if (PlayerSetting.isTunnel() && PlayerSetting.getRender() == 1) setRender(view);
+    }
+
+    private void setCaption(View view) {
+        PlayerSetting.putCaption(!PlayerSetting.isCaption());
+        mBinding.captionText.setText(caption[PlayerSetting.isCaption() ? 1 : 0]);
+    }
+
+    private boolean onCaption(View view) {
+        if (PlayerSetting.isCaption()) startActivity(new Intent(Settings.ACTION_CAPTIONING_SETTINGS));
+        return PlayerSetting.isCaption();
+    }
+
+    private void setAdblock(View view) {
+        Setting.putAdblock(!Setting.isAdblock());
+        mBinding.adblockText.setText(getSwitch(Setting.isAdblock()));
+    }
+
+    private void onBackground(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.player_background).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(background, PlayerSetting.getBackground(), (dialog, which) -> {
+            mBinding.backgroundText.setText(background[which]);
+            PlayerSetting.putBackground(which);
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void setAudioDecode(View view) {
+        PlayerSetting.putAudioPrefer(!PlayerSetting.isAudioPrefer());
+        mBinding.audioDecodeText.setText(getSwitch(PlayerSetting.isAudioPrefer()));
+    }
+
+    private void setVideoDecode(View view) {
+        PlayerSetting.putVideoPrefer(!PlayerSetting.isVideoPrefer());
+        mBinding.videoDecodeText.setText(getSwitch(PlayerSetting.isVideoPrefer()));
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        if (!hidden) initView();
+    }
+}
