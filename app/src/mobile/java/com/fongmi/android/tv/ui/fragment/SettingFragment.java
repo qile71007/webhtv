7bea92311e555c2a33eab7eca3f1549b8b4bb449
qile <jllyzjs@126.com>
2026-06-24 15:42:23 +0800

    Add files via upload

---
+++ app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingFragment.java
@@ -0,0 +1,463 @@
+package com.fongmi.android.tv.ui.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Process;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewbinding.ViewBinding;
+
+import com.fongmi.android.tv.BuildConfig;
+import com.fongmi.android.tv.R;
+import com.fongmi.android.tv.Updater;
+import com.fongmi.android.tv.api.config.LiveConfig;
+import com.fongmi.android.tv.api.config.VodConfig;
+import com.fongmi.android.tv.api.config.WallConfig;
+import com.fongmi.android.tv.bean.Config;
+import com.fongmi.android.tv.bean.Live;
+import com.fongmi.android.tv.bean.Site;
+import com.fongmi.android.tv.databinding.FragmentSettingBinding;
+import com.fongmi.android.tv.db.AppDatabase;
+import com.fongmi.android.tv.event.ConfigEvent;
+import com.fongmi.android.tv.event.RefreshEvent;
+import com.fongmi.android.tv.impl.Callback;
+import com.fongmi.android.tv.impl.ConfigListener;
+import com.fongmi.android.tv.impl.LiveListener;
+import com.fongmi.android.tv.impl.SiteListener;
+import com.fongmi.android.tv.setting.PlayerSetting;
+import com.fongmi.android.tv.setting.Setting;
+import com.fongmi.android.tv.ui.activity.HomeActivity;
+import com.fongmi.android.tv.ui.base.BaseFragment;
+import com.fongmi.android.tv.ui.dialog.AboutDialog;
+import com.fongmi.android.tv.ui.dialog.ConfigDialog;
+import com.fongmi.android.tv.ui.dialog.HistoryDialog;
+import com.fongmi.android.tv.ui.dialog.LiveDialog;
+import com.fongmi.android.tv.ui.dialog.RestoreDialog;
+import com.fongmi.android.tv.ui.dialog.SiteDialog;
+import com.fongmi.android.tv.ui.dialog.ThemeDialog;
+import com.fongmi.android.tv.utils.FileUtil;
+import com.fongmi.android.tv.utils.Notify;
+import com.fongmi.android.tv.utils.PermissionUtil;
+import com.fongmi.android.tv.utils.ResUtil;
+import com.github.catvod.bean.Doh;
+import com.github.catvod.net.OkHttp;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SettingFragment extends BaseFragment implements ConfigListener, SiteListener, LiveListener, ThemeDialog.Listener {
+
+    private FragmentSettingBinding mBinding;
+    private String[] size;
+
+    public static SettingFragment newInstance() {
+        return new SettingFragment();
+    }
+
+    private String getSwitch(boolean value) {
+        return getString(value ? R.string.setting_on : R.string.setting_off);
+    }
+
+    private String getThemeText() {
+        int color = Setting.getThemeColor();
+        if (color == -1) return getString(R.string.setting_off);
+        return getString(color == 0 ? R.string.setting_auto : R.string.setting_custom);
+    }
+
+    private int getDohIndex() {
+        return Math.max(0, VodConfig.get().getDoh().indexOf(Doh.objectFrom(Setting.getDoh())));
+    }
+
+    private String[] getDohList() {
+        List<String> list = new ArrayList<>();
+        for (Doh item : VodConfig.get().getDoh()) list.add(item.getName());
+        return list.toArray(new String[0]);
+    }
+
+    private HomeActivity getRoot() {
+        return (HomeActivity) requireActivity();
+    }
+
+    @Override
+    protected ViewBinding getBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        return mBinding = FragmentSettingBinding.inflate(inflater, container, false);
+    }
+
+    @Override
+    protected void initView() {
+        EventBus.getDefault().register(this);
+        mBinding.vodUrl.setText(VodConfig.getDesc());
+        mBinding.liveUrl.setText(LiveConfig.getDesc());
+        setWallText();
+        mBinding.versionText.setText(BuildConfig.VERSION_NAME);
+        setOtherText();
+        setCacheText();
+    }
+
+    private void setOtherText() {
+        mBinding.themeColorText.setText(getThemeText());
+        mBinding.dohText.setText(getDohList()[getDohIndex()]);
+        mBinding.incognitoText.setText(getSwitch(Setting.isIncognito()));
+        mBinding.sizeText.setText((size = ResUtil.getStringArray(R.array.select_size))[PlayerSetting.getSize()]);
+    }
+
+    private void setCacheText() {
+        FileUtil.getCacheSize(new Callback() {
+            @Override
+            public void success(String result) {
+                mBinding.cacheText.setText(result);
+            }
+        });
+    }
+
+    @Override
+    protected void initEvent() {
+        mBinding.vod.setOnClickListener(this::onVod);
+        mBinding.doh.setOnClickListener(this::setDoh);
+        mBinding.live.setOnClickListener(this::onLive);
+        mBinding.wall.setOnClickListener(this::onWall);
+        mBinding.size.setOnClickListener(this::setSize);
+        mBinding.cache.setOnClickListener(this::onCache);
+        mBinding.backup.setOnClickListener(this::onBackup);
+        mBinding.enhance.setOnClickListener(this::onEnhance);
+        mBinding.player.setOnClickListener(this::onPlayer);
+        mBinding.danmaku.setOnClickListener(this::onDanmaku);
+        mBinding.restore.setOnClickListener(this::onRestore);
+        mBinding.version.setOnClickListener(this::onVersion);
+        mBinding.vod.setOnLongClickListener(this::onVodEdit);
+        mBinding.vodHome.setOnClickListener(this::onVodHome);
+        mBinding.live.setOnLongClickListener(this::onLiveEdit);
+        mBinding.liveHome.setOnClickListener(this::onLiveHome);
+        mBinding.wall.setOnLongClickListener(this::onWallEdit);
+        mBinding.incognito.setOnClickListener(this::setIncognito);
+        mBinding.vodHistory.setOnClickListener(this::onVodHistory);
+        mBinding.themeColor.setOnClickListener(this::onThemeColor);
+        mBinding.liveHistory.setOnClickListener(this::onLiveHistory);
+        mBinding.wallDefault.setOnClickListener(this::setWallDefault);
+        mBinding.wallRefresh.setOnClickListener(this::setWallRefresh);
+        mBinding.wallRefresh.setOnLongClickListener(this::onWallHistory);
+
+        // ===== 新增：重置应用 =====
+        mBinding.resetApp.setOnClickListener(v -> showResetConfirmDialog());
+    }
+
+    @Override
+    public void setConfig(Config config) {
+        if (config.getUrl().startsWith("file")) {
+            requireView().post(() -> PermissionUtil.requestFile(this, allGranted -> load(config)));
+        } else {
+            load(config);
+        }
+    }
+
+    private void load(Config config) {
+        switch (config.getType()) {
+            case 0:
+                VodConfig.load(config, getCallback());
+                break;
+            case 1:
+                LiveConfig.load(config, getCallback());
+                break;
+            case 2:
+                Setting.putWall(0);
+                WallConfig.load(config, getCallback());
+                break;
+        }
+    }
+
+    private Callback getCallback() {
+        return new Callback() {
+            @Override
+            public void start() {
+                Notify.progress(requireActivity());
+            }
+
+            @Override
+            public void success() {
+                Notify.dismiss();
+                setCacheText();
+            }
+
+            @Override
+            public void error(String msg) {
+                Notify.dismiss();
+                Notify.show(msg);
+            }
+        };
+    }
+
+    @Override
+    public void setSite(Site item) {
+        VodConfig.get().setHome(item);
+    }
+
+    @Override
+    public void setLive(Live item) {
+        LiveConfig.get().setHome(item);
+    }
+
+    @Override
+    public void setTheme(int color) {
+        Setting.putThemeColor(color);
+        RefreshEvent.theme();
+    }
+
+    private void onVod(View view) {
+        ConfigDialog.create().vod().show(this);
+    }
+
+    private void onLive(View view) {
+        ConfigDialog.create().live().show(this);
+    }
+
+    private void onWall(View view) {
+        ConfigDialog.create().wall().show(this);
+    }
+
+    private boolean onVodEdit(View view) {
+        ConfigDialog.create().vod().edit().show(this);
+        return true;
+    }
+
+    private boolean onLiveEdit(View view) {
+        ConfigDialog.create().live().edit().show(this);
+        return true;
+    }
+
+    private boolean onWallEdit(View view) {
+        ConfigDialog.create().wall().edit().show(this);
+        return true;
+    }
+
+    private void onVodHome(View view) {
+        SiteDialog.create().search().change().show(this);
+    }
+
+    private void onLiveHome(View view) {
+        LiveDialog.show(this);
+    }
+
+    private void onVodHistory(View view) {
+        HistoryDialog.create().vod().show(this);
+    }
+
+    private void onLiveHistory(View view) {
+        HistoryDialog.create().live().show(this);
+    }
+
+    private void onPlayer(View view) {
+        getRoot().change(2);
+    }
+
+    private void onDanmaku(View view) {
+        getRoot().change(4);
+    }
+
+    private void onEnhance(View view) {
+        getRoot().change(3);
+    }
+
+    private void onThemeColor(View view) {
+        ThemeDialog.show(this);
+    }
+
+
+    private void onVersion(View view) {
+        AboutDialog.show(requireActivity(), () -> Updater.create().force().start(requireActivity()));
+    }
+
+    private void setWallDefault(View view) {
+        Setting.putWall(Setting.nextDefaultWall());
+        Setting.putWallType(0);
+        setWallText();
+        ConfigEvent.wall();
+    }
+
+    private void setWallRefresh(View view) {
+        Setting.putWall(0);
+        WallConfig.get().load(getCallback());
+    }
+
+    private boolean onWallHistory(View view) {
+        HistoryDialog.create().wall().show(this);
+        return true;
+    }
+
+    private void setIncognito(View view) {
+        Setting.putIncognito(!Setting.isIncognito());
+        mBinding.incognitoText.setText(getSwitch(Setting.isIncognito()));
+    }
+
+    private void setSize(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.setting_size).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(size, PlayerSetting.getSize(), (dialog, which) -> {
+            mBinding.sizeText.setText(size[which]);
+            PlayerSetting.putSize(which);
+            RefreshEvent.size();
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void setDoh(View view) {
+        new MaterialAlertDialogBuilder(requireActivity()).setTitle(R.string.setting_doh).setNegativeButton(R.string.dialog_negative, null).setSingleChoiceItems(getDohList(), getDohIndex(), (dialog, which) -> {
+            setDoh(VodConfig.get().getDoh().get(which));
+            dialog.dismiss();
+        }).show();
+    }
+
+    private void setDoh(Doh doh) {
+        OkHttp.dns().setDoh(doh);
+        Setting.putDoh(doh.toString());
+        mBinding.dohText.setText(doh.getName());
+    }
+
+    private void onCache(View view) {
+        FileUtil.clearCache(new Callback() {
+            @Override
+            public void success() {
+                setCacheText();
+            }
+        });
+    }
+
+    private void onBackup(View view) {
+        PermissionUtil.requestFile(this, allGranted -> AppDatabase.backup(new Callback() {
+            @Override
+            public void success() {
+                Notify.show(R.string.backup_success);
+            }
+
+            @Override
+            public void error() {
+                Notify.show(R.string.backup_fail);
+            }
+        }));
+    }
+
+    private void onRestore(View view) {
+        PermissionUtil.requestFile(this, allGranted -> RestoreDialog.create().show(requireActivity(), new Callback() {
+            @Override
+            public void success() {
+                Notify.show(R.string.restore_success);
+                setOtherText();
+                initConfig();
+            }
+
+            @Override
+            public void error() {
+                Notify.show(R.string.restore_fail);
+            }
+        }));
+    }
+
+    // ===== 新增：重置应用相关方法 =====
+
+    /**
+     * 显示重置确认对话框
+     */
+    private void showResetConfirmDialog() {
+        new MaterialAlertDialogBuilder(requireActivity())
+                .setTitle(R.string.reset_confirm_title)
+                .setMessage(R.string.reset_confirm_message)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> resetApp())
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
+    }
+
+    /**
+     * 执行重置操作
+     */
+    private void resetApp() {
+        // 1. 清除 SharedPreferences
+        requireContext().getSharedPreferences("video_settings", Context.MODE_PRIVATE).edit().clear().apply();
+        requireContext().getSharedPreferences("settings", Context.MODE_PRIVATE).edit().clear().apply();
+
+        // 2. 清除数据库（使用正确的单例方法）
+        try {
+            AppDatabase.get().clearAllTables();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 3. 删除缓存和文件
+        deleteRecursive(requireContext().getCacheDir());
+        deleteRecursive(requireContext().getFilesDir());
+
+        // 4. 提示并重启
+        Toast.makeText(requireContext(), R.string.reset_success, Toast.LENGTH_SHORT).show();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = requireContext().getPackageManager()
+                    .getLaunchIntentForPackage(requireContext().getPackageName());
+            if (intent != null) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+            requireActivity().finish();
+            Process.killProcess(Process.myPid());
+        }, 1500);
+    }
+
+    /**
+     * 递归删除文件/文件夹
+     */
+    private void deleteRecursive(File file) {
+        if (file == null || !file.exists()) return;
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    deleteRecursive(child);
+                }
+            }
+        }
+        file.delete();
+    }
+
+    // ===== 重置应用相关方法结束 =====
+
+    private void initConfig() {
+        VodConfig.get().init().load(getCallback());
+        LiveConfig.get().init().load();
+        WallConfig.get().init().load();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onConfigEvent(ConfigEvent event) {
+        if (event.type() == ConfigEvent.Type.WALL) {
+            setWallText();
+            return;
+        }
+        if (event.type() != ConfigEvent.Type.COMMON) return;
+        mBinding.vodUrl.setText(VodConfig.getDesc());
+        mBinding.liveUrl.setText(LiveConfig.getDesc());
+        setWallText();
+    }
+
+    private void setWallText() {
+        mBinding.wallUrl.setText(Setting.getWallDesc(WallConfig.getDesc()));
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        if (hidden) return;
+        setCacheText();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        EventBus.getDefault().unregister(this);
+    }
+}
